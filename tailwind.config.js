51b2c2bbba8287978cb7a7f91ea0094d8b385aed
Kunark Khewal <kunark.khewal88@gmail.com>
2022-05-14 16:28:47 +0530

    tailwind config updated and font-awesome added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'blue': '#1DA1F2',
+        'darkblue': '#2795D9',
+        'lightblue': '#EFF9FF',
+        'dark': '#657786',
+        'light': '#AAB8C2',
+        'lighter': '#E1E8ED',
+        'lightest': '#F5F8FA',
+      }
+    },
   },
   plugins: [],
 }
